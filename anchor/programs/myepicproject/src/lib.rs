e9372567f123998cdffcedce93e5fc8090e80a54
Badisse Bouabdallah <badisse.bouabdallah@proton.me>
2022-10-22 02:15:17 +0200

    Store basic data on our contract

--- anchor/programs/myepicproject/src/lib.rs
+++ anchor/programs/myepicproject/src/lib.rs
@@ -1,5 +1,4 @@
 use anchor_lang::prelude::*;
-use anchor_lang::solana_program::entrypoint::ProgramResult;
 
 declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 
@@ -7,9 +6,41 @@ declare_id!("Fg6PaFpoGXkYsidMpWTK6W2BeZ7FEfcYkg476zPFsLnS");
 pub mod myepicproject {
   use super::*;
   pub fn start_stuff_off(ctx: Context<StartStuffOff>) -> Result <()> {
+    // Get a reference to the account.
+    let base_account = &mut ctx.accounts.base_account;
+    // Initialize total_gifs.
+    base_account.total_gifs = 0;
+    Ok(())
+  }
+
+  pub fn add_gif(ctx: Context<AddGif>) -> Result <()> {
+    // Get a reference to the account and increment total_gifs.
+    let base_account = &mut ctx.accounts.base_account;
+    base_account.total_gifs += 1;
     Ok(())
   }
 }
 
+// Attach certain variables to the StartStuffOff context.
 #[derive(Accounts)]
-pub struct StartStuffOff {}
+pub struct StartStuffOff<'info> {
+    #[account(init, payer = user, space = 9000)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+    pub system_program: Program <'info, System>,
+}
+
+// Specify what data you want in the AddGif Context.
+// Getting a handle on the flow of things :)?
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+  #[account(mut)]
+  pub base_account: Account<'info, BaseAccount>,
+}
+
+// Tell Solana what we want to store on this account.
+#[account]
+pub struct BaseAccount {
+    pub total_gifs: u64,
+}
